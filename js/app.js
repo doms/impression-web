791434d3911e6e79c696acb6039c4b411ecff99b
notdom <domyoung424@gmail.com>
2017-09-30 00:14:37 -0500

    finally get particle.js setup correctly

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 // /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-// particlesJS.load("particles-js", "assets/particlesjs-config.json", function() {
-//   console.log("callback - particles.js config loaded");
-// });
+particlesJS.load("particles-js", "assets/particlesjs-config.json", function() {
+  console.info("[INFO] particles.js config loaded");
+});
