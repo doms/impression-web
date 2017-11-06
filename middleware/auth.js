5931a603c6944618be31a488a7ffa341359a7b61
notdom <domyoung424@gmail.com>
2017-11-06 10:16:38 -0600

    prefer let over var

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const firebase = require("firebase");
 // check current state of whether a user is logged in or not
 module.exports = {
   isAuthenticated: function(req, res, next) {
-    var user = firebase.auth().currentUser;
+    let user = firebase.auth().currentUser;
     if (user !== null) {
       req.user = user;
       next();
