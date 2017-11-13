efbe87c8e4a5b52a1a0e3a3f03d9f481089455a3
notdom <domyoung424@gmail.com>
2017-11-12 18:24:52 -0600

    move function to helpers

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -137,3 +137,51 @@ var submitForm = $("#save-results");
 submitForm.on("click", function() {
   $("#submit-form").submit();
 });
+
+// auth modal
+$(".form")
+  .find("input, textarea")
+  .on("keyup blur focus", function(e) {
+    var $this = $(this),
+      label = $this.prev("label");
+
+    if (e.type === "keyup") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.addClass("active highlight");
+      }
+    } else if (e.type === "blur") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.removeClass("highlight");
+      }
+    } else if (e.type === "focus") {
+      if ($this.val() === "") {
+        label.removeClass("highlight");
+      } else if ($this.val() !== "") {
+        label.addClass("highlight");
+      }
+    }
+  });
+
+$(".tab a").on("click", function(e) {
+  e.preventDefault();
+
+  $(this)
+    .parent()
+    .addClass("active");
+  $(this)
+    .parent()
+    .siblings()
+    .removeClass("active");
+
+  target = $(this).attr("href");
+
+  $(".tab-content > div")
+    .not(target)
+    .hide();
+
+  $(target).fadeIn(600);
+});
