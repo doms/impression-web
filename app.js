de97ce2bcafbde81db876f61237647618b91a2ea
AnenaVictoriaSims <anena.sims@mavs.uta.edu>
2017-11-16 22:27:10 -0600

    Replaced project code with my version of working login functionality. Needs styling

---
+++ app.js
@@ -0,0 +1,65 @@
+(function(){
+
+	// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZ0HSEZD3KJevSHvgXB4rIJxMqvWLvO9I",
+    authDomain: "my-impressions.firebaseapp.com",
+    databaseURL: "https://my-impressions.firebaseio.com",
+    projectId: "my-impressions",
+    storageBucket: "my-impressions.appspot.com",
+    messagingSenderId: "37335912302"
+  };
+  firebase.initializeApp(config);
+
+	// Get all the login elements defined in index.html
+	const txtEmail = document.getElementById('txtEmail');
+	const txtPassword = document.getElementById('txtPassword');
+	const btnLogin = document.getElementById('btnLogin');
+	const btnSignUp = document.getElementById('btnSignUp');
+	const btnLogout = document.getElementById('btnLogout');
+
+	// Add login event for btnLogin button
+	btnLogin.addEventListener('click', e => {
+		// Get email and password field values
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		// Store it to firebase
+		const auth = firebase.auth();
+		// Sign in, returns a promis that verifies user...
+		const promise = auth.signInWithEmailAndPassword(email, pass);
+		//  or catches error
+		promise.catch(e => console.log(e.message));
+
+	});
+
+	// Add signup event for btnSignUp button
+	btnSignUp.addEventListener('click', e => {
+		// Get email and password field values
+		// TODO: CHECK 4 REAL EMAIL!
+		const email = txtEmail.value;
+		const pass = txtPassword.value;		// Checks that passworkd is => 6chars
+		// Store it to firebase
+		const auth = firebase.auth();
+		// Sign in, returns a promis that verifies user...
+		const promise = auth.createUserWithEmailAndPassword(email, pass);
+		//  or catches errors but doesn't catch change in login state
+		promise.catch(e => console.log(e.message));
+	});
+
+	btnLogout.addEventListener('click', e => {
+		firebase.auth().signOut();
+	})
+
+	// Realtime listener to authinticate account and track login state
+	firebase.auth().onAuthStateChanged(firebaseUser =>{
+		if(firebaseUser){
+			console.log('logged in');
+			document.getElementById('btnLogout').style.visibility = 'visible';
+		}
+		else{
+			console.log('not logged in');
+			document.getElementById('btnLogout').style.visibility = 'hidden';
+		}
+	});
+	
+}());
